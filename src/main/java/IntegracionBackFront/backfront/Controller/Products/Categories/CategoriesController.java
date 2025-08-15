e2d9c4d4c59e75f34b008a3087f33166411dd029
astridww <20240476@ricaldone.edu.sv>
2025-08-15 09:55:05 -0600

    cambios de los endpoints

--- src/main/java/IntegracionBackFront/backfront/Controller/Products/Categories/CategoriesController.java
+++ src/main/java/IntegracionBackFront/backfront/Controller/Products/Categories/CategoriesController.java
@@ -5,16 +5,14 @@ import IntegracionBackFront.backfront.Services.Categories.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/category")
+@CrossOrigin
+@RequestMapping("/Apicategory")
 public class CategoriesController {
 
     //Inyectar la clase Service
